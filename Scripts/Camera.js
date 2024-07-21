55a1163ef6d548c45ab6464b03e7b40ad936e702
Kinesis <lordkinesis@naver.com>
2024-07-21 16:00:59 +0900

    :hammer: Add Camera and Player Script.
    Redbrick에서 형상 관리는 아직 지원하지 않으므로 개별적으로 스크립트를 관리하기 위해 게임 내 스크립트를 추가함.
    
    프로젝트 파일(.json)만 업로드 하는 것은 버전 관리 및 스크립트 관리에 있어 큰 불편함이 느껴짐.

---
+++ Scripts/Camera.js
@@ -0,0 +1,23 @@
+// //카메라가 플레이어 위치로부터 떨어진 거리, 로테이션 값
+const CAMERA_POSITION_WEIGHT_Y = 40; 
+const CAMERA_ROTATION_X = -90;
+const CAMERA_ROTATION_Z = 0;
+
+const avatar = REDBRICK.AvatarManager.createDefaultAvatar();
+const camera = WORLD.getObject("MainCamera");
+avatar.setDefaultController();
+
+//카메라 Y값 및 로테이션 초기값 설정.
+camera.position.y = CAMERA_POSITION_WEIGHT_Y;
+camera.rotation.set(toRadians(CAMERA_ROTATION_X), 0, toRadians(CAMERA_ROTATION_Z)); //rotation.set() 메소드는 매개변수를 라디안으로 입력 받음.
+
+// Degree (도)를 Radian (라디안)으로 변환하는 함수.
+function toRadians(degrees) {
+  return degrees * Math.PI / 180;
+}
+
+//매 프레임 마다 카메라의 X와 Z값을 플레이어로부터 일정한 거리로 업데이트 해줌.
+function Update(){
+    camera.position.x = avatar.position.x;
+    camera.position.z = avatar.position.z;
+}
---
+++ Scripts/Player.js
@@ -0,0 +1,112 @@
+let previousPosition = null;
+const ground = WORLD.getObject("Ground");
+const controlAreaSize = 10; // 플레이어의 구역 (해당 구역을 넘어서면 ground 오브젝트가 이동됨).
+const moveDistance = 2; // ground 오브젝트의 이동 간격.
+
+function Start() {
+    // 플레이어의 시작 좌표를 복사함 -> 플레이어의 이동 방향을 알아내기 위함.
+    previousPosition = PLAYER.position.clone();
+}
+
+function Update(dt) {
+    
+}
+/*[Need Fix]
+* 대각선 키(w + a, w + d, s + a, s + d)를 누르고 있다가 한 쪽 키를 때면 키 입력이 무시되는 현상이 발생됨.
+*/
+
+function OnKeyDown(event) {
+    switch (event.key.toLowerCase()) {
+        case 'w':
+        case 'a':
+        case 's':
+        case 'd':
+            getPlayerMovementDirection();
+            break;
+    }
+}
+
+function getPlayerMovementDirection() {
+    let currentPosition = PLAYER.position;
+    let directionVector = currentPosition.clone().sub(previousPosition);
+    let direction;
+
+    if (directionVector.length() > 0) {
+        directionVector.normalize();
+
+        // vector를 기반으로 방향을 결정함.
+        if (directionVector.z > 0.5 && Math.abs(directionVector.x) <= 0.5) {
+            direction = "South";
+        } else if (directionVector.z < -0.5 && Math.abs(directionVector.x) <= 0.5) {
+            direction = "North";
+        } else if (directionVector.x > 0.5 && Math.abs(directionVector.z) <= 0.5) {
+            direction = "East";
+        } else if (directionVector.x < -0.5 && Math.abs(directionVector.z) <= 0.5) {
+            direction = "West";
+        } else if (directionVector.x > 0.5 && directionVector.z > 0.5) {
+            direction = "South-East";
+        } else if (directionVector.x > 0.5 && directionVector.z < -0.5) {
+            direction = "North-East";
+        } else if (directionVector.x < -0.5 && directionVector.z > 0.5) {
+            direction = "South-West";
+        } else if (directionVector.x < -0.5 && directionVector.z < -0.5) {
+            direction = "North-West";
+        }
+
+        console.log("Player Movement Direction:", direction);
+        checkAndMoveGround(direction, currentPosition);
+    }
+
+    // previous position 업데이트.
+    previousPosition.copy(currentPosition);
+}
+
+function checkAndMoveGround(direction, currentPosition) {
+    // controalArea 영역을 벗어 났는지 확인하기.
+    if (Math.abs(currentPosition.x - ground.position.x) > controlAreaSize / 2 ||
+        Math.abs(currentPosition.z - ground.position.z) > controlAreaSize / 2) {
+        
+        // Player가 controalArea를 벗어 났으면 ground 오브젝트를 이동하기.
+        moveGroundObject(direction);
+    }
+}
+
+function moveGroundObject(direction) {
+    let groundPosition = ground.position.clone();
+    groundPosition.x = 0;
+    groundPosition.z = 0;
+    switch (direction) {
+        case "North":
+            groundPosition.z = -moveDistance;
+            break;
+        case "South":
+            groundPosition.z = moveDistance;
+            break;
+        case "East":
+            groundPosition.x = moveDistance;
+            break;
+        case "West":
+            groundPosition.x = -moveDistance;
+            break;
+        case "North-East":
+            groundPosition.x += moveDistance / Math.sqrt(2);
+            groundPosition.z -= moveDistance / Math.sqrt(2);
+            break;
+        case "South-East":
+            groundPosition.x += moveDistance / Math.sqrt(2);
+            groundPosition.z += moveDistance / Math.sqrt(2);
+            break;
+        case "North-West":
+            groundPosition.x -= moveDistance / Math.sqrt(2);
+            groundPosition.z -= moveDistance / Math.sqrt(2);
+            break;
+        case "South-West":
+            groundPosition.x -= moveDistance / Math.sqrt(2);
+            groundPosition.z += moveDistance / Math.sqrt(2);
+            break;
+    }
+
+    console.log(`Moving ground to position: (${groundPosition.x}, ${ground.position.y}, ${groundPosition.z})`);
+    // Player의 이동 방향에 맞춰 ground 오브젝트를 이동 시키기.
+    ground.go(groundPosition.x, ground.position.y, groundPosition.z);
+}
