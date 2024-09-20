17f90cdfc87d81a989fc756324719e077beb4785
Kinesis <lordkinesis@naver.com>
2024-09-20 18:25:33 +0900

    :hammer: Update: GameManager.js

--- Scripts/GameManager.js
+++ Scripts/GameManager.js
@@ -12,17 +12,24 @@ REDBRICK.Signal.addListener("CHECK_NEXT_ROUND", function(params) {
     }
 })
 
+function Start(){
+    REDBRICK.Signal.send("CHECK_GUI_ROUND", {round: 1});
+}
+
 function RoundResetting(){
     GLOBAL.enemyNowCount = 0;
     if(GLOBAL.gameRound == 1){
         GLOBAL.enemyMaxCount = 10;
         GLOBAL.mobSpawnSpeed = 3000;
+        REDBRICK.Signal.send("CHECK_GUI_ROUND", {round: 1});
     }else if(GLOBAL.gameRound == 2){
         GLOBAL.enemyMaxCount = 15;
         GLOBAL.mobSpawnSpeed = 2000;
+        REDBRICK.Signal.send("CHECK_GUI_ROUND", {round: 2});
     }else if(GLOBAL.gameRound == 3){
         GLOBAL.enemyMaxCount = 20;
         GLOBAL.mobSpawnSpeed = 1000;
+        REDBRICK.Signal.send("CHECK_GUI_ROUND", {round: 3});
     }
     GLOBAL.isRoundClear = true;
 }
