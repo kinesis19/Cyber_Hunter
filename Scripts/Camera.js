3bfe552051a1fa766e39bbca12cb35cec11cc1ff
Kinesis <lordkinesis@naver.com>
2025-01-08 09:53:58 +0900

    :hammer: Update: Camera.js
    이전에 레드브릭에서 업데이트한 내용을 repo에도 반영함.

--- Scripts/Camera.js
+++ Scripts/Camera.js
@@ -1,23 +1,25 @@
-// //카메라가 플레이어 위치로부터 떨어진 거리, 로테이션 값
-const CAMERA_POSITION_WEIGHT_Y = 40; 
-const CAMERA_ROTATION_X = -90;
-const CAMERA_ROTATION_Z = 0;
+const CAMERA_POSITION = { x: 0, y: 30, z: 0 }; 
+const CAMERA_ROTATION = { x: -75, y: 0, z: 0 }; 
 
 const avatar = REDBRICK.AvatarManager.createDefaultAvatar();
 const camera = WORLD.getObject("MainCamera");
 avatar.setDefaultController();
 
-//카메라 Y값 및 로테이션 초기값 설정.
-camera.position.y = CAMERA_POSITION_WEIGHT_Y;
-camera.rotation.set(toRadians(CAMERA_ROTATION_X), 0, toRadians(CAMERA_ROTATION_Z)); //rotation.set() 메소드는 매개변수를 라디안으로 입력 받음.
-
-// Degree (도)를 Radian (라디안)으로 변환하는 함수.
+// Convert degrees to radians.
 function toRadians(degrees) {
   return degrees * Math.PI / 180;
 }
 
-//매 프레임 마다 카메라의 X와 Z값을 플레이어로부터 일정한 거리로 업데이트 해줌.
-function Update(){
-    camera.position.x = avatar.position.x;
-    camera.position.z = avatar.position.z;
+// Set camera position and rotation to the specified values.
+camera.position.set(CAMERA_POSITION.x, CAMERA_POSITION.y, CAMERA_POSITION.z);
+camera.rotation.set(
+  toRadians(CAMERA_ROTATION.x), 
+  toRadians(CAMERA_ROTATION.y), 
+  toRadians(CAMERA_ROTATION.z)
+);
+
+// Update function to keep the camera following the avatar's X and Z positions
+function Update() {
+  camera.position.x = avatar.position.x;
+  camera.position.z = avatar.position.z;
 }
